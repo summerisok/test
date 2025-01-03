2a0ff4bb8726153a225afc7349d81c0a6cf9eeac
summerisok <94211752+summerisok@users.noreply.github.com>
2025-01-03 23:41:16 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,16 +1,5 @@
-from http.server import BaseHTTPRequestHandler
-from api.translate import handler
-
-def handle(req):
-    return handler(req)
-
-class Handler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        return handle(self)
-    
-    def do_OPTIONS(self):
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
-        self.end_headers()
+def handler(request):
+    return {
+        "statusCode": 200,
+        "body": "API is running"
+    }
