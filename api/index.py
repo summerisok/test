d8b892d40bc86f32e1ef6300aec47d8f76a35747
summerisok <94211752+summerisok@users.noreply.github.com>
2025-01-04 00:59:55 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,11 @@
 from http.server import BaseHTTPRequestHandler
+import json
 
-def handler(event, context):
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-        },
-        "body": {
-            "message": "Hello from Python!"
-        }
-    }
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        message = "Hello from Python on Vercel!"
+        self.wfile.write(json.dumps({"message": message}).encode())
+        return
