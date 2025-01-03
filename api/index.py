e0ec4fafb90b8e9f6e0987ce0aad23b7a4139f9d
summerisok <94211752+summerisok@users.noreply.github.com>
2025-01-04 00:57:47 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,15 @@
-def handler(request):
+from http.server import BaseHTTPRequestHandler
+
+def handler(event, context):
     return {
-        'statusCode': 200,
-        'body': {
-            'message': 'Hello World'
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+        },
+        "body": {
+            "message": "Hello from Python!"
         }
     }
