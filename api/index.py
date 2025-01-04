52296b590cfa8c77b2c99b061f75384c627801a3
summerisok <94211752+summerisok@users.noreply.github.com>
2025-01-04 12:23:48 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,8 +3,7 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
+        self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = "Hello from Python!"
-        self.wfile.write(str(message).encode())
+        self.wfile.write('Hello, world!'.encode('utf-8'))
         return
